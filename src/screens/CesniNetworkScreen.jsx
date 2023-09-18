2b1bb97d925b1a409608c58b5ab7a5a19b4a574c
MakamNetz <144923469+makamnetz@users.noreply.github.com>
2023-09-18 13:16:57 +0300

    Update CesniNetworkScreen.jsx

--- src/screens/CesniNetworkScreen.jsx
+++ src/screens/CesniNetworkScreen.jsx
@@ -2,35 +2,60 @@ import CesniNetwork3D from '../components/CesniNetwork3d'
 import CesniNetwork2D from '../components/CesniNetwork2d'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import AhenkNavbar from '../components/Navbar'
-import { Container } from 'react-bootstrap'
+import { Container, Spinner } from 'react-bootstrap' // Import Spinner
 import DimensionButtons from '../components/DimensionButtons'
 
-function CesniNetworkScreen({ routing, handleScreen, screen }) {
-  const [nodes, setNodes] = useState([])
-  const [links, setLinks] = useState([])
+const url = 'https://recepgul82.pythonanywhere.com/cesni_all/?format=json'
+
+function CesniNetworkScreen({ handleScreen, screen }) {
+  const [allNodes, setAllNodes] = useState([])
+  const [allLinks, setAllLinks] = useState([])
+  const [selectedNode, setSelectedNode] = useState()
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
-    async function fetchCesniNetwork() {
-      const { data } = await axios.get(routing)
-      setNodes(data.nodes)
-      setLinks(data.links)
+    async function fetchNodesLinks() {
+      try {
+        setIsLoading(true)
+
+        const { data } = await axios.get(url)
+        setAllNodes(data.nodes)
+        setAllLinks(data.links)
+        setIsLoading(false)
+      } catch (error) {
+        console.log(error)
+        setIsLoading(false)
+      }
     }
-    fetchCesniNetwork()
-  }, [routing])
+
+    fetchNodesLinks()
+  }, [])
+
+  function handleSelectedNode(e) {
+    console.log('selected node: ', e)
+  }
 
   return (
     <>
-      <Container fluid style={{ margin: 0 }}>
-        <AhenkNavbar />
+      <Container fluid>
         <DimensionButtons handleScreen={handleScreen} />
-        {screen === '2d' ? (
+        {isLoading ? (
+          <div className='text-center mt-5'>
+            <Spinner animation='border' role='status' variant='info'>
+              <span className='visually-hidden'>Loading...</span>
+            </Spinner>
+          </div>
+        ) : screen === '2d' ? (
           <div>
-            <CesniNetwork2D nodes={nodes} links={links} />
+            <CesniNetwork2D
+              nodes={allNodes}
+              links={allLinks}
+              handleSelectedNode={handleSelectedNode}
+            />
           </div>
         ) : (
           <div>
-            <CesniNetwork3D nodes={nodes} links={links} />
+            <CesniNetwork3D nodes={allNodes} links={allLinks} />
           </div>
         )}
       </Container>
