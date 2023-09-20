5f90763c526ff303e88804c9d98de47878118878
MakamNetz <144923469+makamnetz@users.noreply.github.com>
2023-09-20 21:20:00 +0300

    Update MakamNetworkScreen.jsx

--- src/screens/MakamNetworkScreen.jsx
+++ src/screens/MakamNetworkScreen.jsx
@@ -1,28 +1,106 @@
-import AhenkNavbar from '../components/Navbar'
-import VerticalDropdown from '../components/VerticalDropdownGroup'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import MakamControlDropdown from '../components/MakamControlDropdown'
+import { Container, Row, Col, Spinner } from 'react-bootstrap'
+import RemovableListItems from '../components/RemovableListItems'
+import { MakamTopologyDynamic2D } from '../components/MakamTopology2d'
 
 function MakamNetworkScreen() {
+  const [allNodes, setAllNodes] = useState([])
+  const [allLinks, setAllLinks] = useState([])
+  const [availableMakams, setAvailableMakams] = useState([])
+  const [usedMakams, setUsedMakams] = useState([])
+  const [ALLMAKAMS, setALLMAKAMS] = useState()
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsLoading(true)
+
+      try {
+        const [makamsResponse, nodesLinksResponse] = await Promise.all([
+          axios.get(
+            'https://recepgul82.pythonanywhere.com/makam_all/?format=json'
+          ),
+          axios.get(
+            'https://recepgul82.pythonanywhere.com/cesni_all/?format=json'
+          ),
+        ])
+
+        setAvailableMakams(makamsResponse.data)
+        setALLMAKAMS(makamsResponse.data)
+        setAllNodes(nodesLinksResponse.data.nodes)
+        setAllLinks(nodesLinksResponse.data.links)
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  function handleSelect(e) {
+    if (!usedMakams.find((item) => item.id === parseInt(e))) {
+      const selectedMakam = availableMakams.find(
+        (item) => item.id === parseInt(e)
+      )
+
+      setUsedMakams((prevUsedMakams) => [...prevUsedMakams, selectedMakam])
+      setAvailableMakams((prevAvailableMakams) =>
+        prevAvailableMakams.filter((item) => item.id !== parseInt(e))
+      )
+    }
+  }
+
+  function handleRemove(e) {
+    const removedMakam = ALLMAKAMS.find(
+      (item) => item.id === parseInt(e.target.value)
+    )
+    setUsedMakams(usedMakams.filter((item) => item !== removedMakam))
+    setAvailableMakams([...availableMakams, removedMakam])
+  }
+
   return (
     <>
-      <AhenkNavbar />
-      <div>MakamNetworkScreen</div>
-      <Container fluid>
-        <Row>
-          <Col>
-            <VerticalDropdown title={'makam 1'} />
-          </Col>
-          <Col>
-            <VerticalDropdown title={'makam 2'} />
-          </Col>
-          <Col>
-            <VerticalDropdown title={'makam 3'} />
-          </Col>
-          <Col xs={8}></Col>
-        </Row>
-      </Container>
+      {isLoading ? (
+        <div className='text-center mt-5'>
+          <Spinner animation='border' variant='info' />
+        </div>
+      ) : (
+        <Container fluid>
+          <div className='ms-5 my-1'>MakamNetworkScreen</div>
+
+          <Row>
+            {availableMakams && (
+              <MakamControlDropdown
+                availableMakams={availableMakams}
+                handleSelect={handleSelect}
+              />
+            )}
+          </Row>
+          <Row>
+            <Col lg={2}>
+              {usedMakams.length > 0 && (
+                <RemovableListItems
+                  items={usedMakams}
+                  handleRemove={handleRemove}
+                />
+              )}
+            </Col>
+            <Col lg={10}>
+              {allNodes && (
+                <MakamTopologyDynamic2D
+                  makams={usedMakams}
+                  nodes={allNodes}
+                  links={allLinks}
+                />
+              )}
+            </Col>
+          </Row>
+        </Container>
+      )}
     </>
   )
 }
